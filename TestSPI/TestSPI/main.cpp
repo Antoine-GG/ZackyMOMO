b336696fc7f2450435bbd53a4e9ae908a186a6da
Simard Zachary <2132249@cgodin.qc.ca>
2023-09-01 14:44:32 -0400

    Setup Initial SPI par Zachary

---
+++ TestSPI/TestSPI/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * TestSPI.cpp
+ *
+ * Created: 2023-09-01 13:59:54
+ * Author : 2132249
+ */ 
+
+#define F_CPU 8000000UL
+#include <avr/io.h> /* inclure la librairie de base de définition des pins… */
+#include <util/delay.h> /* inclure la librairie de temporisation */
+
+int main(void)
+{
+    // -------- Inits --------- //
+    DDRB |= (1<<PB1); /* Registre de Direction DDRB : PORT PB1 en sortie:*/
+    
+	
+	while (1){
+		
+    }
+}
